b1855527291da27ec4a949ceb2925350121999d7
Allenyuy <yanallen0213@gmail.com>
2025-05-16 14:23:38 -0400

    Update App.jsx
    comments

--- AIPfrontend/src/App.jsx
+++ AIPfrontend/src/App.jsx
@@ -5,11 +5,11 @@ import ApproveHours from './ApproveHours';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> // create router
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/send-hours" element={<SendHours />} />
-        <Route path="/approve-hours" element={<ApproveHours />} />
+        <Route path="/" element={<Login />} />// path to login page
+        <Route path="/send-hours" element={<SendHours />} />// path to send Hours
+        <Route path="/approve-hours" element={<ApproveHours />} />// path to approveHours
       </Routes>
     </BrowserRouter>
   );
